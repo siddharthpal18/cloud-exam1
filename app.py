b07583752c139400662bdef45325809417deac31
unknown <siddharthpal819@gmail.com>
2025-04-11 16:55:29 -0400

    changed   commit

--- app.py
+++ app.py
@@ -7,12 +7,19 @@ ip_address = socket.gethostbyname(hostname)
 
 @app.route('/')
 def hello_cloud():
-  return 'Hello Cloud!'
+    return 'Welcome to Siddharth's Final Test API Server!'
 
 @app.route('/host')
 def host_name():
-  return hostname
+    return hostname
 
 @app.route('/ip')
 def host_ip():
-  return ip_address
+    return ip_address
+
+@app.route('/health')
+def health():
+    return 'Healthy', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
